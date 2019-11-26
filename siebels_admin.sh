18f88f38e3294d3f1d507c28f6facfd76c2fa5f9
Fabian-Siebels <fabian.siebels@gmail.com>
2019-11-26 21:29:45 +0100

    Vers 0.1.1 = Hinzufügen der Zeitzoneneinstellungen

--- siebels_admin.sh
+++ siebels_admin.sh
@@ -6,7 +6,7 @@
 # Version: 0.1
 
 #   Funktionen
-# - Anzeige Betriebssystem
+# - Aendern der Zeitzone
 
 
 # Farbendeklaration
@@ -29,4 +29,39 @@ function rot() {
 function gelb() {
   echo -e "\\033[33;1m${*}\\033[0m"
 }
+
+#--------------------------------------------------------------------------
+# Auswahl der Zeitzone Ja/Nein
+# Wenn Ja function zeitzoney() Wenn Nein function auswahlfenster()
+function zeitzoneyn() {
+    echo ""
+    rot "Soll Berlin (MEZ / CEST) als Zeitzone eingestellt werden?"
+    read -p "Auswahl [y/n]:" auswahl
+    case "$auswahl" in
+        y) ja
+            ;;
+        n) auswahlfenster
+            ;;
+        *) echo "Ueberpruefe deine Eingabe!"
+            ;;
+    esac
+}
+
+# Einstellen der Zeitzone über einen Link zur Zeitzoneninfo
+# Es wird ein Backup der localtime erstellt, falls etwas schiefgeht
+function zeitzoney() {
+    echo ""
+    sudo mv /etc/localtime /etc/localtime.bak
+    sudo ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+    gruen "Die Zeitzone wurde eingestellt!"
+    date
+    rot "Druecke eine beliebige Taste um zum Auswahlfenster zu wechseln"
+    read -p "" auswahl
+    case "$auswahl" in
+        *) auswahlfenster
+            ;;
+    esac
+}
+
+#--------------------------------------------------------------------------
 
